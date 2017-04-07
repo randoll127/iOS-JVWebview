98d5b219f0e4886b63fbf9e86e4b85a132d2c8a3
fengchao <fengchao@fengchao.local>
2017-04-07 13:36:05 +0800

    Init

---
+++ JVWebview/JVWebview.h
@@ -0,0 +1,19 @@
+//
+//  JVWebview.h
+//  JVWebview
+//
+//  Created by fengchao on 2017/4/7.
+//  Copyright © 2017年 FC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JVWebview.
+FOUNDATION_EXPORT double JVWebviewVersionNumber;
+
+//! Project version string for JVWebview.
+FOUNDATION_EXPORT const unsigned char JVWebviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JVWebview/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+source 'git@github.com:CocoaPods/Specs.git' 
+target 'JVWebview' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for pod-demo
+  pod 'ObjectiveSugar'
+  pod 'ReactiveCocoa', '~> 2.5.0'
+end
